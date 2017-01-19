ccb5daf1171a353c124ee7d8e562748abf18a977
Wayne Chang <wayne66@gmail.com>
2017-01-19 12:53:25 +0800

    analyze verified

---
+++ lololodash/analyze.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var _ = require('lodash')
+
+var worker = function(foo) {
+    
+    var bar = {}
+    
+	var avgOfIncome = _.reduce(foo, function(sumOfIncome, freelancer) {
+        return sumOfIncome + freelancer.income
+    }, 0) / foo.length
+    
+    bar.average = avgOfIncome
+
+    var underperformAry = _
+    .chain(foo)
+    .filter(function(freelancer) {
+        return freelancer.income <= avgOfIncome        
+    })
+    .sortBy('income')
+
+    bar.underperform = underperformAry
+
+    var overperformAry = _
+    .chain(foo)
+    .filter(function(freelancer) {
+        return freelancer.income > avgOfIncome        
+    })
+    .sortBy('income')
+    
+    bar.overperform = overperformAry
+
+    return bar
+}
+
+module.exports = worker
